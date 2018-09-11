402e8cdd9fe964f12057871da4cb15470ed9a35f
lainex <ruslan4401@yandex.ru>
2018-09-11 20:15:37 +0500

    Сделал 2 и 3 задание

--- l1_АхметгареевРуслан.playground/Contents.swift
+++ l1_АхметгареевРуслан.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+// 1 задание
+
 var a: Int =  3 //коэффициент a (x^2), если нет, то вводим 0
 var b: Int =  6 //коэффициент b (x), если нет, то вводим 0
 var c: Int =  2 //коэффициент c (число), если нет, то вводим 0
@@ -20,3 +22,32 @@ if(d < 0){
     x2 = -b - Int(sqrt(Double(d))) / 2 * a
     print(" x1 =",x1,"\n","x2 =",x2)
 }
+
+// 2 задание
+
+var katet1: Double = 4
+var katet2: Double = 3
+var gipotenyza: Double = 0
+var P: Double = 0
+var S: Double = 0
+
+gipotenyza = sqrt(katet1 * katet1 + katet2 * katet2)
+P = katet1 + katet2 + gipotenyza
+S = 0.5 * katet1 * katet2
+print("Гипотенуза = ",gipotenyza)
+print("Периметр = ",P)
+print("Площадь = ",S)
+
+// 3 задание
+
+var Vklad: Int = 5000
+var Procent: Int = 10
+var Srok: Int = 5
+var Sum: Int = 0
+var i: Int = 0
+
+while (i<Srok) {
+    Sum = Vklad + Vklad*Procent
+    i = i + 1
+}
+print("Сумма через",Srok,"лет:",Sum)
